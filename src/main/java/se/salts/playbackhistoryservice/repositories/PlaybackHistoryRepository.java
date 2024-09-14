468c14aa0b6d209f682e2aa4f284a2d102ea9536
AlexPaok4/git <alexandrossaltsidis@gmailcom>
2024-09-14 12:45:06 +0200

    2024-09-14-Alexandros1

--- src/main/java/se/salts/playbackhistoryservice/repositories/PlaybackHistoryRepository.java
+++ src/main/java/se/salts/playbackhistoryservice/repositories/PlaybackHistoryRepository.java
@@ -1,20 +1,17 @@
 package se.salts.playbackhistoryservice.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.salts.playbackhistoryservice.entities.PlaybackHistory;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PlaybackHistoryRepository extends JpaRepository<PlaybackHistory, Long> {
     List<PlaybackHistory> findByUserId(Long userId);
 
-    @Query("SELECT ph FROM PlaybackHistory ph WHERE ph.user.id = :userId ORDER BY COUNT(ph.media) DESC")
-    Optional<PlaybackHistory> findMostPlayedByUserId(Long userId);
-
-    Optional<PlaybackHistory> findMostPlayedMediaByUserId(Long userId);
+    @Query("SELECT ph FROM PlaybackHistory ph WHERE ph.user.id = :userId ORDER BY ph.progress DESC")
+    List<PlaybackHistory> findMostPlayedByUserId(Long userId, Pageable pageable);
 }
-
