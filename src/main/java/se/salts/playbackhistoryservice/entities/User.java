98ef45ba7b1abe91264135d359e27fb1c3378505
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-09-30 19:36:58 +0200

    Lots of changes

--- src/main/java/se/salts/playbackhistoryservice/entities/User.java
+++ src/main/java/se/salts/playbackhistoryservice/entities/User.java
@@ -12,7 +12,6 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
     @Column(name = "username", nullable = false, length = 100)
